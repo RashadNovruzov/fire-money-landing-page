89ace64d9728d375496ea29ec7fa0c257ebf4e23
RashadNovruzov <r4shad.novruz0v@gmail.com>
2022-09-11 23:27:29 +0400

    everything is ready

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,35 @@ $(document).ready(function(){
 		$("body").toggleClass("hidden");
 		$(this).toggleClass("rotate");
 	})
+
+	var sum = document.querySelector('#credit__sum');
+	var time = document.querySelector('#credit__time');
+	var init_sum = new Powerange(sum, { min: 1000, max: 100000, start: 0 });
+	var init_time = new Powerange(time,{ min: 3, max: 30, start: 0 });
+
+	
+	$(".credit__sum-number").text(sum.value);
+	$(".credit__money-get .info__number").text(sum.value+" ₽");
+	sum__get = parseInt(sum.value)+5000;
+	$(".credit__money-return .info__number").text(sum__get+" ₽");
+	sum.onchange=function(){
+		$(".credit__sum-number").text(sum.value);
+		$(".credit__money-get .info__number").text(sum.value+" ₽");
+		sum__get = parseInt(sum.value)+5000;
+		$(".credit__money-return .info__number").text(sum__get+" ₽");
+	}
+
+	$(".credit__date-number").text(time.value);
+	time.onchange=function(){
+		$(".credit__date-number").text(time.value);
+	}
+	
+	$(".questions__item").click(function(){
+		$(this).toggleClass("open");
+		$(this).find(".questions__item-body").slideToggle();
+		$(".questions__item").not($(this)).removeClass("open");
+		$(".questions__item").not($(this)).find(".questions__item-body").slideUp();
+	})
+
 })
+
